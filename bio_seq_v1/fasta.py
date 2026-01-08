866162c71f7b64a85b46f40a2d43d7851e0607cc
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-01-09 02:21:35 +0530

    Add FASTA parser

---
+++ bio_seq_v1/fasta.py
@@ -0,0 +1,28 @@
+#defining our parser function
+def parser(x):
+    all_seq =[]
+    current_id = None
+    current_seq = []
+    valid = set("ACGTUNRYSWKMBDHV-.")
+
+    with open(x, 'r') as fasta: 
+        for line in fasta: 
+            line = line.strip() 
+
+            if not line:
+                continue 
+
+            if line.startswith(">"): 
+                if current_id is not None: 
+                    all_seq.append({"id": current_id, "sequence": "".join(current_seq)}) 
+                current_id = line[1:] 
+                current_seq = [] 
+            else:
+                seq_line = line.upper()
+                if all(base in valid for base in seq_line):
+                     current_seq.append(seq_line)
+
+        if current_id is not None: 
+                    all_seq.append({"id": current_id, "sequence": "".join(current_seq)}) 
+                    current_seq = []
+    return all_seq            
